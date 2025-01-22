8ed816a25a0b34060c2ce748fdc8d8063fc73da5
iminparallel <ch.harit1995@gmail.com>
2025-01-22 22:16:48 +0530

    removed docker

--- auth.js
+++ auth.js
@@ -12,7 +12,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL:
+        "https://url-shortner-production-4ca2.up.railway.app/google/callback",
       passReqToCallback: true,
     },
     async function (request, accessToken, refreshToken, profile, done) {
